b3cec4280f729e2b5b1f85611f81b1551872db38
Brian Kemper <bkemper@gmail.com>
2016-05-11 23:40:43 -0600

    got underlining to work for section

--- src/mainCtrl.js
+++ src/mainCtrl.js
@@ -5,10 +5,36 @@ angular.module('app').controller('mainCtrl', function($scope, $document, $window
         $document.scrollToElement(el, 50, 200);
     };
 
+    var aboutOff = angular.element(document.getElementById('about'))[0].offsetTop;
+    var skillsOff = angular.element(document.getElementById('skills'))[0].offsetTop;
+
     $scope.pxScr = 0;
+    document.getElementById('navAbout').style.textDecoration = 'underline';
+    $scope.navStyle = {'color': '#ebeaea'};
     $document.on('scroll', function() {
         $scope.$apply(function() {
             $scope.pxScr = $window.scrollY;
+            if ($scope.pxScr > 75) {
+                $scope.navStyle = {'color': '#131829'}
+            } else {
+                $scope.navStyle = {'color': '#ebeaea'};
+            }
+            if ($scope.pxScr > aboutOff - 65 && $scope.pxScr < skillsOff - 65) {
+                document.getElementById('navAbout').style.textDecoration = 'underline';
+            } else {
+                document.getElementById('navAbout').style.textDecoration = 'none';
+            }
+            if ($scope.pxScr > skillsOff - 65) {
+                document.getElementById('navSkills').style.textDecoration = 'underline';
+            } else {
+                document.getElementById('navSkills').style.textDecoration = 'none';
+            }
+            if ($window.innerHeight + $document[0].body.scrollTop >= $document[0].body.scrollHeight - 50) {
+                document.getElementById('navContact').style.textDecoration = 'underline';
+                document.getElementById('navSkills').style.textDecoration = 'none';
+            } else {
+                document.getElementById('navContact').style.textDecoration = 'none';
+            }
         });
     });
 
